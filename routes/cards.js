e28071ace94276ef1bdaa9b46e05e393f2117b28
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-10-23 09:15:50 -0700

    adds routes for getting next random card, handling for no side in the query

--- routes/cards.js
+++ routes/cards.js
@@ -3,21 +3,33 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+  const numberOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numberOfCards);
+  res.redirect(`/cards/${flashcardId}`);
+});
+
 
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
+
+  if ( !side ) {
+    return res.redirect(`/cards/${id}?side=question`);
+  }
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { id, text };
+  const name = req.cookies.username;
+
+  const templateData = { id, text, name };
 
   if (side === 'question') {
     templateData.hint = hint;
-    templateData.sideToShow = 'answer'
-    templateData.sideToShowDisplay = 'Answer'
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
   } else if (side === 'answer') {
-    templateData.sideToShow = 'question'
-    templateData.sideToShowDisplay = 'Question'
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
   }
 
   res.render('card', templateData);
